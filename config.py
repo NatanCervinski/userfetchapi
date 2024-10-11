ce0b824b388d8461a9ffd276873b4e9811b6768f
NatanCervinski <natanscer@gmail.com>
2024-10-11 01:32:20 -0300

    mudado o env padrão para sqlite

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ settings = Dynaconf(
     settings_files=["settings.toml", ".secrets.toml"],
     environments=True,
     load_dotenv=True,
-    default_env="MYSQL",
+    default_env="SQLITE",
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
